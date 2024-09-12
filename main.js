538f132f92487cbe91aec589f185d8e3d3e932f4
San Vert <itsanvert@gmail.com>
2024-09-12 23:45:30 +0700

    first commit

--- main.js
+++ main.js
@@ -48,3 +48,125 @@ function tick() {
 }
 
 tick();
+// jQuery for typewriter effect
+$(document).ready(function () {
+  var typeSkills = [
+    "UX | UI Designer.",
+    "Programmer.",
+    "Graphic Designer.",
+    "Web Developer.",
+  ];
+  var skillIndex = 0;
+  var charIndex = 0;
+  var currentText = "";
+  var isDeleting = false;
+
+  function type() {
+    if (isDeleting) {
+      currentText = typeSkills[skillIndex].substring(0, charIndex--);
+    } else {
+      currentText = typeSkills[skillIndex].substring(0, charIndex++);
+    }
+
+    $(".typewrite").text(currentText);
+
+    if (!isDeleting && charIndex === typeSkills[skillIndex].length) {
+      isDeleting = true;
+      setTimeout(type, 2000); // Pause after typing full word
+    } else if (isDeleting && charIndex === 0) {
+      isDeleting = false;
+      skillIndex = (skillIndex + 1) % typeSkills.length; // Loop through skills
+      setTimeout(type, 500);
+    } else {
+      setTimeout(type, isDeleting ? 50 : 100);
+    }
+  }
+
+  type();
+});
+
+// jQuery for smooth scroll
+$('a[href^="#"]').on("click", function (event) {
+  event.preventDefault();
+  var target = this.hash;
+  $("html, body").animate(
+    {
+      scrollTop: $(target).offset().top,
+    },
+    1000
+  ); // 1000 ms animation
+});
+
+// jQuery for portfolio filtering
+$(".filter").on("click", function () {
+  var category = $(this).data("category");
+
+  $(".box").hide(); // Hide all projects
+
+  if (category === "all") {
+    $(".box").show(); // Show all projects if 'all' is clicked
+  } else {
+    $('.box[data-category="' + category + '"]').show(); // Show filtered projects
+  }
+});
+// jQuery for typewriter effect
+$(document).ready(function () {
+  var typeSkills = [
+    "UX | UI Designer.",
+    "Programmer.",
+    "Graphic Designer.",
+    "Web Developer.",
+  ];
+  var skillIndex = 0;
+  var charIndex = 0;
+  var currentText = "";
+  var isDeleting = false;
+
+  function type() {
+    if (isDeleting) {
+      currentText = typeSkills[skillIndex].substring(0, charIndex--);
+    } else {
+      currentText = typeSkills[skillIndex].substring(0, charIndex++);
+    }
+
+    $(".typewrite").text(currentText);
+
+    if (!isDeleting && charIndex === typeSkills[skillIndex].length) {
+      isDeleting = true;
+      setTimeout(type, 2000); // Pause after typing full word
+    } else if (isDeleting && charIndex === 0) {
+      isDeleting = false;
+      skillIndex = (skillIndex + 1) % typeSkills.length; // Loop through skills
+      setTimeout(type, 500);
+    } else {
+      setTimeout(type, isDeleting ? 50 : 100);
+    }
+  }
+
+  type();
+});
+
+// jQuery for smooth scroll
+$('a[href^="#"]').on("click", function (event) {
+  event.preventDefault();
+  var target = this.hash;
+  $("html, body").animate(
+    {
+      scrollTop: $(target).offset().top,
+    },
+    1000
+  ); // 1000 ms animation
+});
+
+// jQuery for portfolio filtering
+$(".filter").on("click", function () {
+  var category = $(this).data("category");
+
+  $(".box").hide(); // Hide all projects
+
+  if (category === "all") {
+    $(".box").show(); // Show all projects if 'all' is clicked
+  } else {
+    $('.box[data-category="' + category + '"]').show(); // Show filtered projects
+  }
+});
